04e5b2fb2f18878a7d3b76c32ceaaa42f06ee80a
SuperXRanzo <okanskyavenger@gmail.com>
2024-03-28 20:22:43 +0700

    void input

--- QuickShort_0038/QuickShort_0038.cpp
+++ QuickShort_0038/QuickShort_0038.cpp
@@ -4,3 +4,16 @@ using namespace std;
 int arr[20];
 int cmp_count = 0;
 int mov_count = 0;
+int n;
+
+void input() {
+    while (true)
+    {
+        cout << "Masukan panjang elemnet array: ";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+            cout << "\nMaksimum panjang array adalah 20" << endl;
+    }
